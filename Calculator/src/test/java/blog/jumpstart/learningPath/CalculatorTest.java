8af4ff4f0654d35e2e4d217d67e45edb8cb5c588
azaboklicka <azaboklicka@PL-N100IM7.gtk.gtech.com>
2017-07-04 15:51:58 +0200

    extract calculator

--- Calculator/src/test/java/blog/jumpstart/learningPath/CalculatorTest.java
+++ Calculator/src/test/java/blog/jumpstart/learningPath/CalculatorTest.java
@@ -5,34 +5,36 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	private static final Calculator CALCULATOR = new Calculator();
+
 	@Test
 	public void sum() {
-		Assert.assertEquals(3, (new Calculator()).add(1, 2));
+		Assert.assertEquals(3, CALCULATOR.add(1, 2));
 	}
 
 	@Test
 	public void difference() {
-		Assert.assertEquals(3, (new Calculator()).subtract(5, 2));
+		Assert.assertEquals(3, CALCULATOR.subtract(5, 2));
 	}
 
 	@Test
 	public void product() {
-		Assert.assertEquals(10, (new Calculator()).multiply(5, 2));
+		Assert.assertEquals(10, CALCULATOR.multiply(5, 2));
 	}
 
 	@Test
 	public void quotient() {
-		Assert.assertEquals(3, (new Calculator()).divide(6, 2));
+		Assert.assertEquals(3, CALCULATOR.divide(6, 2));
 	}
 
 	@Test
 	public void remainder() {
-		Assert.assertEquals(1, (new Calculator()).modulo(7, 2));
+		Assert.assertEquals(1, CALCULATOR.modulo(7, 2));
 	}
 
 	@Test
 	public void power() {
-		Assert.assertEquals(8, (new Calculator()).modulo(2, 3));
+		Assert.assertEquals(8, CALCULATOR.modulo(2, 3));
 	}
 
 }
