78097f633bd3592faead6f259c8289e00b59828c
azaboklicka <azaboklicka@PL-N100IM7.gtk.gtech.com>
2017-07-04 15:58:18 +0200

    extract calculator and correct power test

--- Calculator/src/test/java/blog/jumpstart/learningPath/CalculatorTest.java
+++ Calculator/src/test/java/blog/jumpstart/learningPath/CalculatorTest.java
@@ -5,36 +5,40 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	private static final Calculator CALCULATOR = new Calculator();
-
 	@Test
 	public void sum() {
-		Assert.assertEquals(3, CALCULATOR.add(1, 2));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(3, calculator.add(1, 2));
 	}
 
 	@Test
 	public void difference() {
-		Assert.assertEquals(3, CALCULATOR.subtract(5, 2));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(3, calculator.subtract(5, 2));
 	}
 
 	@Test
 	public void product() {
-		Assert.assertEquals(10, CALCULATOR.multiply(5, 2));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(10, calculator.multiply(5, 2));
 	}
 
 	@Test
 	public void quotient() {
-		Assert.assertEquals(3, CALCULATOR.divide(6, 2));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(3, calculator.divide(6, 2));
 	}
 
 	@Test
 	public void remainder() {
-		Assert.assertEquals(1, CALCULATOR.modulo(7, 2));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(1, calculator.modulo(7, 2));
 	}
 
 	@Test
 	public void power() {
-		Assert.assertEquals(8, CALCULATOR.modulo(2, 3));
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(8, calculator.power(2, 3));
 	}
 
 }
