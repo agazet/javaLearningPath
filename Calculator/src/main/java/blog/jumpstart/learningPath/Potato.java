50aece2d433c4a6745231ee771ac08f13295ea55
azaboklicka <azaboklicka@PL-N100IM7.gtk.gtech.com>
2017-07-06 00:09:58 +0200

    change the code to look as written in the adventure

--- Calculator/src/main/java/blog/jumpstart/learningPath/Potato.java
+++ Calculator/src/main/java/blog/jumpstart/learningPath/Potato.java
@@ -13,21 +13,8 @@ public class Potato {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		Calculator calculator = takeCalculator();
-		
-		int sum = calculator.add(1, 2);
-
-		sayCalculationResult(sum);
-
-	}
-	
-	static Calculator takeCalculator(){
-		return new Calculator();
-	}
-	
-	static void sayCalculationResult(int whatPotatoCalculated){
-		System.out.println("The calculation I did gives back: " + whatPotatoCalculated);
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.add(1, 2));
 	}
 
 }
